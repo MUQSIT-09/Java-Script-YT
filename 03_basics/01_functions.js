08d4549c19aebb5d594088e535df2507998681b3
MUQSIT-09 <149312251+MUQSIT-09@users.noreply.github.com>
2025-03-18 07:16:54 +0000

    Functions in JS

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,51 +1,50 @@
-// //Functions in Js:
-// function sayMyName(){
-//     console.log("H");
-//     console.log("I");
-//     console.log("T");
-//     console.log("E");
-//     console.log("S");
-//     console.log("H");
-// }
+Functions in Js:
 
-// sayMyName() //function define
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
 
-
-// // function addTwoNumbers(number1, number2){ // consists of parameters
-
-// //     console.log(number1 + number2); // if return statement not given the resukt value is not stored
-// // }
-// //   const result = addTwoNumbers(3, 5) // Here 3,5 are arguments 
-
-// //   console.log("Result: ", result); // o/p ---> 8 , Result:  undefined
-
-// //   addTwoNumbers()//function define
-// //   addTwoNumbers(5,6)//function call , consists of arguments
-// function addTwoNumbers(number1, number2){
-    
-//     let result = number1 + number2
-//     return result
-//     return number1 + number2 // can't execute anything after return.
-// }
-
-// const result = addTwoNumbers(3, 5) // Here 3,5 are arguments 
-
-// console.log("Result: ", result); // o/p----> Result: 8
+sayMyName() //function define
 
 
-// function loginUserMessage(username = "sam"){ // Give the deafult value as sam. ifa ny other value comes it ovrwrite sam.
-//     if(!username){ // or if(username === undifined ) ---> inside if null,undifined are false always.
-//         console.log("PLease enter a username");
-//         return
-//     }
-//     return `${username} just logged in`
-//     // After return statement nothing gets executed.
-// }
-// loginUserMessage("Shah")    // Thsi will execute the finction but would not print anything as it is not having console statement.
-// console.log(loginUserMessage(""))       // o/p --> just logged in (empty string when given) without if statement
-// console.log(loginUserMessage(null))     // o/p -----> undefined just logged in. if default value is not given.
-// console.log(loginUserMessage(null))     // o/p -----> null just logged in. if default value is not given.
-// console.log(loginUserMessage("Azam"))   // o/p ---> Azam just logged in
+function addTwoNumbers(number1, number2){ // consists of parameters
+  console.log(number1 + number2); // if return statement not given the resukt value is not stored
+ }
+  const result = addTwoNumbers(3, 5) // Here 3,5 are arguments 
+  console.log("Result: ", result); // o/p ---> 8 , Result:  undefined
+  addTwoNumbers()//function define
+  addTwoNumbers(5,6)//function call , consists of arguments
+
+function addTwoNumbers(number1, number2){
+    let result = number1 + number2
+    return result
+    return number1 + number2 // can't execute anything after return.
+}
+
+const result = addTwoNumbers(3, 5) // Here 3,5 are arguments 
+
+console.log("Result: ", result); // o/p----> Result: 8
+
+
+function loginUserMessage(username = "sam"){ // Give the deafult value as sam. ifa ny other value comes it ovrwrite sam.
+    if(!username){ // or if(username === undifined ) ---> inside if null,undifined are false always.
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+    // After return statement nothing gets executed.
+}
+
+loginUserMessage("Shah")    // Thsi will execute the finction but would not print anything as it is not having console statement.
+console.log(loginUserMessage(""))       // o/p --> just logged in (empty string when given) without if statement
+console.log(loginUserMessage(null))     // o/p -----> undefined just logged in. if default value is not given.
+console.log(loginUserMessage(null))     // o/p -----> null just logged in. if default value is not given.
+console.log(loginUserMessage("Azam"))   // o/p ---> Azam just logged in
 
 
 
@@ -53,6 +52,7 @@
 // ++++++++++++++++++++++++++++++++++++++++++++++
 
 // ...num1 is a rest operator same as spread operator only
+
 function calculateCartPrice(val1, val2, ...num1){
     return num1
 }
@@ -67,9 +67,11 @@ const user = {
 function handleObject(anyobject){
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`); //Username is sam and price is 399
 }
+
 // if price is given instead of prices
 // handleObject(user)
 // without defining object directly
+
 handleObject({
     username: "sam",
     price: 399
@@ -77,9 +79,9 @@ handleObject({
 
 const myNewArray = [200, 400, 100, 600]
 
-function returnSecondValue(getArray){ //you can taje any name over here as your parameters.
+function returnSecondValue(getArray){  //you can take any name over here as your parameters.
     return getArray[1]
 }
 
-// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue(myNewArray));
 console.log(returnSecondValue([200, 400, 500, 1000]));
